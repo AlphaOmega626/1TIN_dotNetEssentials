d982e13f1ba70526925cb6f5bb0835cd70df64bb
Frankie Claessens <claessensfrankie@gmail.com>
2015-02-05 14:38:02 +0100

    no message

---
+++ H2_Cedric/H2O5/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace H2O5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnA_MouseMove(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("U ging over de knop.");
+        }
+
+        private void btnA_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("U ging over de knop.");
+        }
+
+       
+    }
+}
---
+++ H2_Cedric/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace H2O1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJa_Click(object sender, RoutedEventArgs e)
+        {
+            lblA.Content = "Ja";
+            lblB.Content = "Ja";
+            lblC.Content = "Ja";
+
+        }
+
+        private void btnNee_Click(object sender, RoutedEventArgs e)
+        {
+            lblA.Content = "Nee";
+            lblB.Content = "Nee";
+            lblC.Content = "Nee";
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            lblA.Content = "A";
+            lblB.Content = "B";
+            lblC.Content = "C";
+        }
+    }
+}
