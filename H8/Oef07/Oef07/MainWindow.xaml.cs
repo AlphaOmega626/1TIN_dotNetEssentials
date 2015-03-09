de429e7baa43b273a04acefbcd80151caebde795
Frankie Claessens <claessensfrankie@gmail.com>
2015-03-09 18:40:36 +0100

    no message

---
+++ H8/Oef07/Oef07/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oef07
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+        
+    {
+        int einde;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tekenButton_Click(object sender, RoutedEventArgs e)
+        {
+            tabelTextBlock.Text = "";
+            if (!intTextBox.Text.Equals(null))
+            {
+                einde = Convert.ToInt32(intTextBox.Text);
+            }
+            if (einde > 0 && einde < 16)
+            {
+                for (int rij = 1; rij <= einde; rij++)
+                {
+                    for (int kolom = 1; kolom <= einde; kolom++)
+                    {
+                        //if (rij == 1 && kolom == 1)
+                        //{
+                        //    for (int i = 1; i <= einde; i++)
+                        //    {
+                        //        tabelTextBlock.Text += String.Format("{0}{1}", "\t", i);
+                        //    }
+                        //    tabelTextBlock.Text += "\n\n";
+                        //}
+                        int product = rij * kolom;
+                        tabelTextBlock.Text += String.Format("{0}{1}{2}", rij, "\t", product);
+
+                    }
+                    tabelTextBlock.Text += "\n";
+                }
+               
+            }
+            else
+            {
+                MessageBox.Show("Vul een getal > 0 en <= 15 in!");
+            }            
+        }
+    }
+}
