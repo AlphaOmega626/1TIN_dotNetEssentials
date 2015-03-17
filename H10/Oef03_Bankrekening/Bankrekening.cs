30cff6dce6dc9d71b90c6cd024b46651fb3b6655
Frankie Claessens <claessensfrankie@gmail.com>
2015-03-17 21:34:50 +0100

    Wekker en Bankrekening (classes)

---
+++ H10/Oef03_Bankrekening/Bankrekening.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oef03_Bankrekening
+{
+    class Bankrekening
+    {
+        private double saldo;
+
+        public Bankrekening()
+        {
+            this.saldo = 0.0;
+        }
+
+        public double getSaldo()
+        {
+                return this.saldo;
+        }
+
+        public void transaction(double amount)
+        {
+            if (amount < 0)
+            {
+                this.saldo -= Math.Abs(amount);
+            }
+            else
+            {
+                this.saldo += Math.Abs(amount);
+            }
+            
+        }
+    }
+}
---
+++ H10/Oef03_Bankrekening/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oef03_Bankrekening
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Bankrekening bankrek;
+        public MainWindow()
+        {
+            InitializeComponent();
+            bankrek = new Bankrekening();
+            saldoTextBox.Content = String.Format("{0:C}", bankrek.getSaldo());
+        }
+
+        private void transactionButton_Click(object sender, RoutedEventArgs e)
+        {
+            bankrek.transaction(Convert.ToDouble(amountTextBox.Text));
+            saldoTextBox.Content = String.Format("{0:C}", bankrek.getSaldo());
+            if (bankrek.getSaldo() < 0)
+            {
+                MessageBox.Show("NEGATIEF SALDO!");
+            }
+        }
+    }
+}
