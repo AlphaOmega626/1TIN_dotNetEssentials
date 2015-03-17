afd38c2f0e0e1525a18529b8b7a9824edf066837
Frankie Claessens <claessensfrankie@gmail.com>
2015-03-17 14:24:23 +0100

    no message

---
+++ H10/Oef08_Wekker/Oef08_Wekker/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Oef08_Wekker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Wekker wekker;
+        private DispatcherTimer timer1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            wekker = new Wekker();
+            timer1 = new DispatcherTimer();
+            timer1.Interval = TimeSpan.FromMilliseconds(1000);
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+        }
+
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            currentTimeLabel.Content = wekker.getTijd.ToString("HH:mm:ss");
+        }
+
+        private void setAlarmButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ H10/Oef08_Wekker/Oef08_Wekker/Wekker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Oef08_Wekker
+{
+    class Wekker
+    {
+        private DateTime tijd = new DateTime();
+        private DateTime alarm = new DateTime();
+        private int alarmduur;
+        private DispatcherTimer timer = new DispatcherTimer();
+
+        public Wekker () {
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            tijd = DateTime.Now;
+        }
+
+        public DateTime getTijd {
+            get {
+                return tijd;
+            }
+        }
+
+        public void setAlarm (DateTime alarm)
+        {
+                this.alarm = alarm;
+        }
+
+        public void setAlarmDuur(int alarmduur)
+        {
+            this.alarmduur = alarmduur;
+
+        }
+    }
+}
